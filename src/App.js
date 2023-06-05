a19f0ee73f58bc6bc9303d5e93e5ac7ca8e7743a
kimquaresma <tkdeo9090@gmail.com>
2023-06-05 21:05:45 +0900

    practice network

--- src/App.js
+++ src/App.js
@@ -2,40 +2,35 @@ import React, {useState} from 'react';
 import axios from "axios";
 
 const App = () => {
+    //1.데이터 담을 그릇 설정하기.
     const [articles, setArticles] = useState([])
 
+    //2.데이터 가져오는 함수
     const getArticles = async () => {
-        //
         try {
-
-
-            //무조건 1순위로 실행됨
-            //async - await : 한세트로 동작 ( async가 있으면 await가 있어야함) 비동기, 너무 빠르니 한템포 쉬어가자는 뜻
+            //5. 네트워킹
             const address = "https://newsapi.org/v2/everything?q=tesla&from=2023-05-05&sortBy=publishedAt&apiKey=f1c87e2bb86248de9f9492e513f93e1f"
-            const result = await axios.get(address)
-            console.log("++++++++++++++++++++++++++++++", result.data.articles)
-
-            setArticles(result.data.articles)
-
-
+            const alldata = await axios.get(address)
+            console.log("++++++", alldata.data.articles)
+            setArticles(alldata.data.articles)
         } catch (err) {
-            // try에서 에러 발생되면 catch로 실행됨.
-            // err은 모든 에러에 대한 상수로 표현
-            console.log("----------------------", err.message)
-
+            //3. 에러 핸들링 하기.
+            console.log("err", err.message)
         }
     }
 
     return (
         <div>
+            {/*4. 데이터 display*/}
             <h1>{articles.length}</h1>
             {articles && articles.map(article => (
                 <div>
                     <h1>{article.title}</h1>
-                    <h3>{article.content}</h3>
+                    <h3>{article.description}</h3>
+                    <h4>{article.publishedAt}</h4>
                 </div>
             ))}
-            <button onClick={getArticles}>기사 가져오기</button>
+            <button onClick={getArticles}>데이터 가져오기</button>
         </div>
     );
 };
